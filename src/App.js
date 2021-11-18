c20c4ba49479460bf754cd910faec6e0c0dbd28f
wahlnich <wahlnich@gmail.com>
2021-11-18 10:12:21 -0500

    Site title/favicon and more data

--- src/App.js
+++ src/App.js
@@ -13,6 +13,16 @@ function App() {
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet dictum sit amet justo donec enim. Viverra nam libero justo laoreet sit amet cursus sit. Lectus vestibulum mattis ullamcorper velit. Id volutpat lacus laoreet non curabitur gravida. Imperdiet dui accumsan sit amet nulla facilisi. Tristique senectus et netus et malesuada fames ac. Nisl tincidunt eget nullam non nisi est sit amet facilisis. Risus pretium quam vulputate dignissim. Pharetra pharetra massa massa ultricies mi quis hendrerit. Sagittis nisl rhoncus mattis rhoncus urna neque viverra. Euismod lacinia at quis risus. Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum sociis.',
     },
+    {
+      title: 'Stormlight Archive',
+      author: 'Brandon Sanderson',
+      description: `These glowy dudes with depression, man. They're flying, they're lashing, they're creating diagrams. In the name of Honor, of course.`,
+    },
+    {
+      title: 'The Cloud Roads',
+      author: 'Martha Wells',
+      description: `Awesome flying shapeshifting monster/human hybrids. Reverse gender roles brah!`,
+    },
   ];
 
   // Decide when to show the modal
